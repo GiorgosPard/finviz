a6aabdc2811a9ae41f6d24f1093fb0f07329bf2f
GiorgosPard <83656845+GiorgosPard@users.noreply.github.com>
2022-01-22 18:53:42 +0000

    Update works.py

--- works.py
+++ works.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
+import datetime
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
@@ -15,7 +16,7 @@ import time
 import streamlit as st
 
 
-# In[ ]:
+# In[2]:
 
 
 @st.cache(allow_output_mutation=True,suppress_st_warning=True)
@@ -24,7 +25,7 @@ def place():
     return(placeholder)
 
 
-# In[ ]:
+# In[3]:
 
 
 def loop():
@@ -192,7 +193,7 @@ def loop():
         elif i in construction:
             group.append('Construction')
         elif i in real_estate:
-            group.append('real_estate')
+            group.append('Real Estate')
         elif i in financial:
             group.append('Financial')
         elif i in consumer_and_retail:
@@ -243,28 +244,83 @@ def loop():
 # In[ ]:
 
 
-starttime = time.time()
+
+
+
+
+
+# In[ ]:
+clock=datetime.datetime.now()
+strclock=str(clock).split(' ')[1]
+start = '07:15:00.000000'
+finish ='16:45:00.000000'
+
+finish_tf=strclock>finish
+start_tf=strclock<start
+
+while finish_tf or start_tf:
+    time.sleep(900)
+    clock=datetime.datetime.now()
+    strclock=str(clock).split(' ')[1]
+    finish_tf=strclock>finish
+    start_tf=strclock<start
+    print('sleeping')
+
 c=0
-while True:
     
+
+
+
+while True:
+        
+        
     print("tick")
     data=loop()
+    print("tack")
+    clock=datetime.datetime.now()
+    strclock=str(clock).split(' ')[1]
+    start = '08:15:00.000000'
+    finish ='16:45:00.000000'
+
+    finish_tf=strclock>finish
+    start_tf=strclock<start
+    if (datetime.datetime.today().weekday()==4  and finish==True):
+        st.title('Market is Closed')
+        time.sleep(230400)
+    clock=datetime.datetime.now()
+    strclock=str(clock).split(' ')[1]
+    start = '08:15:00.000000'
+    finish ='16:45:00.000000'
+        
+
+    while finish_tf or start_tf:
+        st.title('Market is Closed')
+        time.sleep(900)
+        clock=datetime.datetime.now()
+        strclock=str(clock).split(' ')[1]
+        finish_tf=strclock>finish
+        start_tf=strclock<start
+        print('sleeping')
+
+
     price_=data.price.tolist()
     change_percentage_=data.change_percentage.tolist()
     ticker_=data.ticker.tolist()
-    fig=px.treemap(data, path=[px.Constant('market'),'group','ticker'],values = 'capitalization', color = 'change_percentage',color_continuous_scale=['Red','Red','Red','Red','Red','Red','Red','crimson','firebrick','gray','Green','limegreen','limegreen','lime','lime','lime','lime','lime','lime'],color_continuous_midpoint=0)
+    fig=px.treemap(data, path=[px.Constant('Athens Stock Exchange'),'group','ticker'],values = 'capitalization', color = 'change_percentage',color_continuous_scale=['Red','Red','Red','Red','Red','Red','Red','crimson','firebrick','gray','Green','limegreen','limegreen','lime','lime','lime','lime','lime','lime'],color_continuous_midpoint=0)
     fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
     fig.update_traces(marker_line=dict(color="black"))
     fig.update_traces(marker_line_width = 0.5)
     fig.data[0].customdata = np.column_stack([price_, change_percentage_,ticker_])
     fig.data[0].texttemplate = "Ticker:%{customdata[2]}<br>Price:$%{customdata[0]}<br>Percentage Change:%{customdata[1]:.2f}%"
     with place().container():
-        st.header('Greek Stock Market Map')
+        st.header('Athens Stock Exchange Tree Map')
+        st.subheader("This is a map that shows the Greek stock market's performance. The map is being refreshed every 10 minutes.")
+        st.write('To view in full screen, hover your mouse over the map and click on the expand arrows on the top right corner')
         st.plotly_chart(fig, use_container_width=True)
+        st.write('last update at: '+str(clock))
         st.write(c)
-        st.write("This is a map that shows the Greek stock market's performance. \n The map is being refreshed every 10 minutes.")
     c=c+1
+    print(c)
     
     
-    time.sleep(600.0 - ((time.time() - starttime) % 600.0))
-
+    time.sleep(600)
