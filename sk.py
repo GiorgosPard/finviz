14ff6c8f548fa7018cac4b119b630b7ee9fbe23d
GiorgosPard <83656845+GiorgosPard@users.noreply.github.com>
2022-01-22 22:20:21 +0000

    Add files via upload

--- sk.py
+++ sk.py
@@ -24,6 +24,32 @@ def place():
     placeholder=st.empty()
     return(placeholder)
 
+@st.cache(allow_output_mutation=True,suppress_st_warning=True)
+def place1():
+    placeholder1=st.empty()
+    return(placeholder1)
+
+
+def current():
+    day=datetime.datetime.today().weekday()
+    if day==4 and str(datetime.datetime.now().time)>'22:20:00.000000':
+        status1=True
+    else:
+        status1=False
+    return(status1)
+        
+def hours():
+    clock=datetime.datetime.now().time
+    if str(clock)>'22:25:00.000000' or str(clock)<'08:15:00.000000':
+        status2=True
+    else:
+        status2=False
+        return(status2)
+
+
+
+
+
 
 # In[3]:
 
@@ -244,81 +270,39 @@ def loop():
 # In[ ]:
 
 
-
-
-
-
-
-# In[ ]:
-clock=datetime.datetime.now()
-strclock=str(clock).split(' ')[1]
-start = '07:15:00.000000'
-finish ='16:45:00.000000'
-
-finish_tf=strclock>finish
-start_tf=strclock<start
-
-while finish_tf or start_tf:
-    time.sleep(900)
-    clock=datetime.datetime.now()
-    strclock=str(clock).split(' ')[1]
-    finish_tf=strclock>finish
-    start_tf=strclock<start
-    print('sleeping')
-
-c=0
-    
-
-
-
 while True:
-        
-        
+    c=0
     print("tick")
-    data=loop()
-    print("tack")
-    clock=datetime.datetime.now()
-    strclock=str(clock).split(' ')[1]
-    start = '08:15:00.000000'
-    finish ='16:45:00.000000'
 
-    finish_tf=strclock>finish
-    start_tf=strclock<start
-    if (datetime.datetime.today().weekday()==4  and finish==True):
-        st.title('Market is Closed')
-        time.sleep(230400)
-    clock=datetime.datetime.now()
-    strclock=str(clock).split(' ')[1]
-    start = '08:15:00.000000'
-    finish ='16:45:00.000000'
-        
-
-    while finish_tf or start_tf:
-        st.title('Market is Closed')
+    if current()==True:
+        place1().text('Market is closed')
+        time.sleep(232,200)
+        print('deep-sleep')
+    elif hours()==True:
+        place1().text('Market is closed')
         time.sleep(900)
-        clock=datetime.datetime.now()
-        strclock=str(clock).split(' ')[1]
-        finish_tf=strclock>finish
-        start_tf=strclock<start
-        print('sleeping')
+        print('normal-sleep')
+    else:
+        place1().text('Market is open')
+        data=loop()
+        print('tack')
 
-
-    price_=data.price.tolist()
-    change_percentage_=data.change_percentage.tolist()
-    ticker_=data.ticker.tolist()
-    fig=px.treemap(data, path=[px.Constant('Athens Stock Exchange'),'group','ticker'],values = 'capitalization', color = 'change_percentage',color_continuous_scale=['Red','Red','Red','Red','Red','Red','Red','crimson','firebrick','gray','Green','limegreen','limegreen','lime','lime','lime','lime','lime','lime'],color_continuous_midpoint=0)
-    fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
-    fig.update_traces(marker_line=dict(color="black"))
-    fig.update_traces(marker_line_width = 0.5)
-    fig.data[0].customdata = np.column_stack([price_, change_percentage_,ticker_])
-    fig.data[0].texttemplate = "Ticker:%{customdata[2]}<br>Price:$%{customdata[0]}<br>Percentage Change:%{customdata[1]:.2f}%"
-    with place().container():
-        st.header('Athens Stock Exchange Tree Map')
-        st.subheader("This is a map that shows the Greek stock market's performance. The map is being refreshed every 10 minutes.")
-        st.write('To view in full screen, hover your mouse over the map and click on the expand arrows on the top right corner')
-        st.plotly_chart(fig, use_container_width=True)
-        st.write('last update at: '+str(clock))
-        st.write(c)
+        price_=data.price.tolist()
+        change_percentage_=data.change_percentage.tolist()
+        ticker_=data.ticker.tolist()
+        fig=px.treemap(data, path=[px.Constant('Athens Stock Exchange'),'group','ticker'],values = 'capitalization', color = 'change_percentage',color_continuous_scale=['Red','Red','Red','Red','Red','Red','Red','crimson','firebrick','gray','Green','limegreen','limegreen','lime','lime','lime','lime','lime','lime'],color_continuous_midpoint=0)
+        fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+        fig.update_traces(marker_line=dict(color="black"))
+        fig.update_traces(marker_line_width = 0.5)
+        fig.data[0].customdata = np.column_stack([price_, change_percentage_,ticker_])
+        fig.data[0].texttemplate = "Ticker:%{customdata[2]}<br>Price:$%{customdata[0]}<br>Percentage Change:%{customdata[1]:.2f}%"
+        with place().container():
+            st.header('Athens Stock Exchange Tree Map')
+            st.subheader("This is a map that shows the Greek stock market's performance. The map is being refreshed every 10 minutes.")
+            st.write('To view in full screen, hover your mouse over the map and click on the expand arrows on the top right corner')
+            st.plotly_chart(fig, use_container_width=True)
+            st.write('last update at: '+str(datetime.datetime.now()))
+            st.write(c)
     c=c+1
     print(c)
     
